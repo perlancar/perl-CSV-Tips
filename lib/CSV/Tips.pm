e92bdc7252a658a4b545fd0c70c2c89157e877ae
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-06-06 16:28:35 +0700

    Initial commit

---
+++ lib/CSV/Tips.pm
@@ -0,0 +1,38 @@
+package CSV::Tips;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Tips on handling CSV (and TSV) using Perl
+
+=head1 DESCRIPTION
+
+This page collects tips on handling CSV (and TSV) using Perl.
+
+
+=head1 CONVFROM (Convert From Other Formats)
+
+
+
+
+=head1 CONVTO (Convert To Other Formats)
+
+
+
+
+=head1 SELCOL (Selecting Columns)
+
+
+
+
+=head1 SELROW (Selecting Rows)
+
+
+
+
+=head1 SEE ALSO
+
+Some prominet CSV modules: L<Text::CSV>, L<Text::CSV_XS>
+
+L<CSV::Examples>
